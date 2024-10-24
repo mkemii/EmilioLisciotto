d3214b1c879a08dc8bd6f5ad069a6411b3994113
Emi Lisci8 <emiprogrammazione@gmail.com>
2024-10-24 15:44:27 +0200

    Changed file structure + Links & QoL changes

---
+++ src/code/script.js
@@ -0,0 +1,22 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById('copy').innerHTML = year;
+
+// Get the button
+let mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
